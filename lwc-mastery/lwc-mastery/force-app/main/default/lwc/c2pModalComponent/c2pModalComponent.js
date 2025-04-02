f940fb4c4a2d2633d10dfa5a0502cc432be8dfc3
varsha0501 <khatvani.varsha@yahoo.com>
2025-04-02 16:23:49 +0530

    dev(lwc) : c2p communicate event with data & bubble event

--- lwc-mastery/lwc-mastery/force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
+++ lwc-mastery/lwc-mastery/force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
@@ -1,13 +1,20 @@
 import { LightningElement } from 'lwc';
-import LightningModal from 'lightning/modal';
-
 
 export default class C2pModalComponent extends LightningElement {
 
     closeHandler(){
         console.log('close handler child called...');
-        const myEvent = new CustomEvent('close');
+        const myEvent = new CustomEvent('close', {
+            bubbles:true,
+            detail : {
+                msg : "Modal Closed Successfully!"
+            }
+        });
         this.dispatchEvent(myEvent);
+    }
+
+    footerHandler(){ 
+        console.log("footerHandler called");
     }
 
 }
--- lwc-mastery/lwc-mastery/force-app/main/default/lwc/c2pParentComponent/c2pParentComponent.js
+++ lwc-mastery/lwc-mastery/force-app/main/default/lwc/c2pParentComponent/c2pParentComponent.js
@@ -3,14 +3,15 @@ import { LightningElement } from 'lwc';
 export default class C2pParentComponent extends LightningElement {
 
     showModal = false;
-
+    msg;
     clickHandler(){
          this.showModal = true
     }
 
-    closeHandler(){
-        console.log('close handler parent called..');
+    closeHandler(event){
+        console.log('close handler parent called..', event);
         this.showModal = false
+        this.msg = event.detail.msg ; 
     }
 
 }
